dc8df7591823dad905efacca0b69dd59bc966b68
Zeguo Li <hamsupgorgor@outlook.com>
2023-05-10 20:44:37 +1000

    WebApp half done (Data visualisation)

--- cancer.py
+++ cancer.py
@@ -11,7 +11,6 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.model_selection import GridSearchCV
@@ -25,175 +24,194 @@ from lime import lime_tabular
 ## Load the Cancer Dataset
 fileName = 'Cancer_Data.csv'
 cancer_dataset = pd.read_csv(fileName)
+raw_dataset = pd.read_csv(fileName)
 
-
-def main():
-    EDA()
-    cleaned_dataset = dataVisualisasion()
-    X, y, scaled_X_train, scaled_X_test, y_train, y_test = dataPreperation(cleaned_dataset)
-    modelTraining(scaled_X_train, scaled_X_test, y_train, y_test)
-    models = modelComparison(scaled_X_train, y_train)
-    model_best = hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test)
-    loaded_model = slModel(model_best, scaled_X_test, y_test)
-    perInter(scaled_X_train, scaled_X_test, loaded_model, X)
 
 # Classification report and Confusion matrix
-def clas_report_CM(y_test, y_pred):
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    print("Precision:",metrics.precision_score(y_test, y_pred, average = 'weighted'))
-    print("Recall:",metrics.recall_score(y_test, y_pred, average = 'weighted'))
-    print("F1-score:",metrics.f1_score(y_test, y_pred, average = 'weighted'))
+def class_report_CM(y_test, y_pred):
+
+    accuracy = "Accuracy: "+ str(metrics.accuracy_score(y_test, y_pred))
+    precision = "Precision: "+ str(metrics.precision_score(y_test, y_pred, average = 'weighted'))
+    recall = "Recall: "+ str(metrics.recall_score(y_test, y_pred, average = 'weighted'))
+    f1Score = "F1-score: "+ str(metrics.f1_score(y_test, y_pred, average = 'weighted'))
+    clrp = classification_report(y_test, y_pred)
+    result = accuracy + "\n" + precision + "\n" + recall + "\n" + f1Score + "\n" + clrp
+    print(result)
     print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
+
+    return result
     
 
 ## Exploratory data analysis
-def EDA():
-    # Data head, Example of what the data looks like
-    print(cancer_dataset.head(5))
+# Data explore - First five rows
+first5Row = cancer_dataset.head(5)
+print(first5Row)
 
-    # shape
-    print(cancer_dataset.shape)
+# shape
+print(cancer_dataset.shape)
 
-    # Check data integrity (missing data)
-    cancer_dataset.info()
+# Check data integrity (missing data)
+cancer_dataset.info()
+checkForNull = cancer_dataset.isnull().sum()
+print(checkForNull)
 
-    # Distribution of benign and malignant case
-    print(cancer_dataset.groupby('diagnosis').size())
+# Distribution of benign and malignant case
+distBM = cancer_dataset.groupby('diagnosis').size()
+print(distBM)
 
-    #List of attributes
-    print(cancer_dataset.keys())
+#List of attributes
+print(cancer_dataset.keys())
+
 
 ## Data Visualisation
 
-def dataVisualisasion():
-    #Drop unnecessary data entry for visualisaiton and re-examine the dataset
-    cancer_dataset.drop(['id', 'Unnamed: 32'], axis = 1, inplace = True)
-    print(cancer_dataset.columns)
-    print(cancer_dataset.shape)
 
-    # Replace string B&M to binary 0, 1
-    cancer_dataset['diagnosis'] = cancer_dataset['diagnosis'].replace({'B': 0, 'M':1})
+# Drop unnecessary data entry for visualisaiton and re-examine the dataset
+cancer_dataset.drop(['id', 'Unnamed: 32'], axis = 1, inplace = True)
+# Check dataset after modification
+print(cancer_dataset.columns)
+print(cancer_dataset.shape)
 
-    # Heat map
-    plt.subplots(figsize=(20, 20)) #Set size of the heat map
-    sns.heatmap(cancer_dataset[cancer_dataset.columns].corr(), annot=True)
-    plt.title("Correlation Matrix")
-    plt.show()
+# Replace string B&M to binary 0, 1
+cancer_dataset['diagnosis'] = cancer_dataset['diagnosis'].replace({'B': 0, 'M':1})
 
-    # Distribution of each attribute
-    cancer_dataset.hist()
-    plt.show()
-    return cancer_dataset
+# Heat map
+heat_map, ax = plt.subplots(figsize=(20, 20)) #Set size of the heat map
+sns.heatmap(cancer_dataset[cancer_dataset.columns].corr(), annot=True, ax = ax)
+plt.title("Correlation Matrix")
+plt.show()
+
+# Distribution of each attribute
+distplot = plt.figure(figsize = (28,21))
+for feature in range(len(cancer_dataset.columns)):
+    plt.subplot(6, 6, feature + 1)
+    sns.histplot(data = cancer_dataset, x = cancer_dataset.columns[feature], hue = 'diagnosis' )
 
 ## Preparing the data for analysis
+# Seperate feature and target set
+X = cancer_dataset.drop('diagnosis', axis = 1) #Everything except the diagnosis column
+y = cancer_dataset['diagnosis'] # The diagnosis column
 
-def dataPreperation(cancer_dataset):
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-    X = cancer_dataset.drop('diagnosis', axis = 1) #Everything except the diagnosis column
-    y = cancer_dataset['diagnosis'] # The diagnosis column
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-    # Feature scaling
-    scaler = StandardScaler()
-    scaled_X_train = scaler.fit_transform(X_train)
-    scaled_X_test = scaler.transform(X_test)
-    return X, y, scaled_X_train, scaled_X_test, y_train, y_test
+# Feature scaling
+scaler = StandardScaler()
+scaled_X_train = scaler.fit_transform(X_train)
+scaled_X_test = scaler.transform(X_test)
 
-def modelTraining(scaled_X_train, scaled_X_test, y_train, y_test):
-    model_with_scaled_data = SVC()
-    model_with_scaled_data.fit(scaled_X_train, y_train)
-    new_y_pred = model_with_scaled_data.predict(scaled_X_test)
-    clas_report_CM(y_test, new_y_pred)
+model_with_scaled_data = SVC()
+model_with_scaled_data.fit(scaled_X_train, y_train)
+new_y_pred = model_with_scaled_data.predict(scaled_X_test)
+class_report_CM(y_test, new_y_pred)
 
 
 ## Using k-fold cross validation to test out different model and avoid over-fitting
-def modelComparison(scaled_X_train, y_train):
-    #Model collection
-    models = []
-    models.append(('KNN', KNeighborsClassifier()))
-    models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
-    models.append(('SVM', SVC(gamma='auto')))
 
-    results = []
-    names = []
-    for name, model in models:
-        kfold = KFold(n_splits=10, random_state=42, shuffle=True)
-        cv_results = cross_val_score(model, scaled_X_train, y_train, cv=kfold, scoring='accuracy')
-        results.append(cv_results)
-        names.append(name)
-        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-        print(msg)
+# Model collection - Spot Check
+models = []
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
 
-    # Compare Algorithms
-    fig = plt.figure()
-    fig.suptitle('Algorithm Comparison')
-    ax = fig.add_subplot(111)
-    plt.boxplot(results)
-    ax.set_xticklabels(names)
-    plt.show()
-    return models
+results = []
+names = []
+message = ""
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=42, shuffle=True)
+    cv_results = cross_val_score(model, scaled_X_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    message += msg + '\n'
+    print(msg)
+
+# Compare Algorithms
+compare_algorithm = plt.figure()
+compare_algorithm.suptitle('Algorithm Comparison')
+ax = compare_algorithm.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+    # return models
 
 ## Hyper parameter tuning for the best result
 
-def hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test):
-    # hyper parameter of current better model
-    better_model = models[3][1]
-    params = better_model.get_params()
-    print(params)
-    # Current hyperprameter config
-    """{'C': 1.0, 'break_ties': False, 'cache_size': 200,
+# def hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test):
+# hyper parameter of current better model
+better_model = models[3][1]
+params = better_model.get_params()
+print(params)
+# Current hyperprameter config
+"""{'C': 1.0, 'break_ties': False, 'cache_size': 200,
     'class_weight': None, 'coef0': 0.0, 'decision_function_shape': 'ovr',
     'degree': 3, 'gamma': 'auto', 'kernel': 'rbf', 'max_iter': -1,
-        'probability': False, 'random_state': None, 'shrinking': True, 'tol': 0.001,
-        'verbose': False}"""
-    # Find optimal hyper parameter for this model
-    params_grid = {'C':[10, 5, 1, 0.5, 0.1, 0.01],
-                    'gamma':[0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1],
-                    'kernel': ['rbf'],
-                    'probability': [True]}
-    grs = GridSearchCV(better_model, params_grid, refit = True, verbose = 1, cv = 20)
-    grs.fit(scaled_X_train, y_train)
+    'probability': False, 'random_state': None, 'shrinking': True, 'tol': 0.001,
+    'verbose': False}"""
 
-    # Display hyper parameter changes
-    print ("\n Best Hyper Parameters: ", grs.best_params_,"\n", "Best estimator: ", grs.best_estimator_,"\n")
 
-    # Train model with hyper parameter tuned
-    model_best = grs.best_estimator_
-    y_prediction = model_best.predict(scaled_X_test)
-    clas_report_CM(y_test, y_prediction)
-    return model_best
+# Since GridSearchCV() is a slow module to run, for the sake of speed,
+# I will put the gridsearch result of the function in params_grid in input so it runs
+# a lot more faster.
+# The intended params_gird looks like this
+# params_grid = {'C':[10, 5, 1, 0.5, 0.1, 0.01],
+#                 'gamma':[0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1],
+#                 'kernel': ['rbf'],
+#                 'probability': [True]}
+# Output = Fitting 20 folds for each of 42 candidates, totalling 840 fits
+# Best Hyper Parameters:  {'C': 10, 'gamma': 0.01, 'kernel': 'rbf', 'probability': True} 
+# Best estimator:  SVC(C=10, gamma=0.01, probability=True)
+
+
+# Find optimal hyper parameter for this model
+params_grid = {'C':[10],
+                'gamma':[0.01],
+                'kernel': ['rbf'],
+                'probability': [True]}
+grs = GridSearchCV(better_model, params_grid, refit = True, verbose = 1, cv = 20)
+grs.fit(scaled_X_train, y_train)
+
+# Display hyper parameter changes
+tuned_result = grs.best_estimator_
+tune_result = "\n Best Hyper Parameters: ", grs.best_params_,"\n", "Best estimator: ", grs.best_estimator_,"\n"
+print (tune_result)
+
+# Train model with hyper parameter tuned
+model_best = grs.best_estimator_
+y_prediction = model_best.predict(scaled_X_test)
+class_report_CM(y_test, y_prediction)
+    # return model_best
 
 
 ## Save and load the trained model
-def slModel(model_best, scaled_X_test, y_test):
-    saveFile = 'Best_SVM_Model.sav'
-    joblib.dump(model_best, saveFile)
-    # Load the model to make sure it is saved properly
-    loaded_model = joblib.load(saveFile)
-    final_y_pred = loaded_model.predict(scaled_X_test)
-    clas_report_CM(y_test, final_y_pred)
-    cm = confusion_matrix(y_test, final_y_pred)
-    disp = ConfusionMatrixDisplay(confusion_matrix = cm)
-    disp.plot()
-    plt.show()
-    return loaded_model
 
-# Performance interpretation with lime, with example
+saveFile = 'Best_SVM_Model.sav'
+joblib.dump(model_best, saveFile)
+
+# Load the model to make sure it is saved properly
+loaded_model = joblib.load(saveFile)
+final_y_pred = loaded_model.predict(scaled_X_test)
+report = class_report_CM(y_test, final_y_pred)
+cm = confusion_matrix(y_test, final_y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm)
+disp.plot()
+plt.savefig('Confusion_Matrix.png')
+plt.show()
 
 
-def perInter(scaled_X_train, scaled_X_test, loaded_model, X):
-    exp = lime_tabular.LimeTabularExplainer(scaled_X_train, 
-                                            class_names= ['benign', 'maglinant'], 
-                                            feature_names= X,
-                                            mode = 'classification' )
+# Performance interpretation using lime,
+# with example of the third data in the test set
 
-    explanation = exp.explain_instance(scaled_X_test[2], loaded_model.predict_proba,
-                                    num_features = len(X),
-                                    top_labels = 2)
 
-    explanation.as_pyplot_figure()
-    plt.show()
+exp = lime_tabular.LimeTabularExplainer(scaled_X_train, 
+                                        class_names= ['benign', 'maglinant'], 
+                                        feature_names= X,
+                                        mode = 'classification' )
 
-if __name__ == '__main__':
-    main()
+explanation = exp.explain_instance(scaled_X_test[2], loaded_model.predict_proba,
+                                num_features = len(X),
+                                top_labels = 2)
+
+explanation.as_pyplot_figure()
+plt.show()
--- webAppSVMCancer.py
+++ webAppSVMCancer.py
@@ -1,6 +1,11 @@
-import streamlit as sl
+# Use this command in terminal to start streamlit!
+# streamlit run webAppSVMCancer.py
 
+import streamlit as sl
 from cancer import *
+from PIL import Image
+
+
 
 ## streamlit page
 
@@ -9,14 +14,42 @@ sl.title("Cancer Cell Prediction")
 sl.markdown("Predict if cell is cancerous based on its geometric features")
 
 # Two tabs for data visualisation and prediction program
-dsTab, ceTab = sl.tabs(
-    [
-        "Data Showcase",
-        "Cell estimator"
-    ]
-)
+dsTab, ceTab = sl.tabs(["Data Showcase","Cell estimator"])
 
 with dsTab:
     sl.header("Cancer dataset")
+    sl.write(raw_dataset)
+    sl.subheader("Check for missing value")
+    sl.write(checkForNull)
+
+    sl.subheader("Number of benign and malignant cases in this dataset")
+    sl.write(distBM)
+
+
+    sl.header("Cancer dataset after cleaning")
+    sl.text("Dropped ID and Blank column, convert diagnosis into binary data")
     sl.write(cancer_dataset)
+
+    sl.header("Heat Map - Correlation Matrix")
+    sl.pyplot(heat_map)
+
+    sl.header("Distribution of dataset features and target")
+    sl.pyplot(distplot)
+
+    sl.header("Algorithm vs. Dataset")
+    sl.text("Four algorithm was chosen for comparison.\nK Nearest Neighbour, Decision Tree, Naive Bayes, and Support Vector Machine.")
+    sl.pyplot(compare_algorithm)
+    sl.text(message)
+
+    sl.header("Hyper parameter tuning result")
+    sl.text(tuned_result)
+    sl.subheader("Classification Report")
+    sl.text(report)
+    sl.subheader("Confusion Matrix of the model")
+    image = Image.open('Confusion_Matrix.png')
+    sl.image(image)
+
+
+
+
 
