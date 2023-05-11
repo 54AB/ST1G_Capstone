4a6edaa8f4c830868e8f4cfcab60dd56b2834a57
Zeguo Li <hamsupgorgor@outlook.com>
2023-05-11 16:27:00 +1000

    All work done

--- cancer.py
+++ cancer.py
@@ -1,7 +1,26 @@
+## Author : Zeguo Li
+## Date Created: 25 April 2023
+## Date Last changed 11 May 2023
 
-# Use Command (py -m pip install "package name") to install packages in VScode
-# https://www.youtube.com/watch?v=ThU13tikHQw how to create virtual environment on VSCode
-# How to chang execution policy if virtual environment cannot be initialisedhttps://stackoverflow.com/questions/18713086/virtualenv-wont-activate-on-windows
+## This program is an attempt to create a machine learning model that predicts the state of tumor cell using dataset Cancer - Kaggle
+# Target Variable: B - Benign, M - Malignant
+# Features number: 30 (Radius, Smoothness, Concavity etc.)
+# Input: Cancer_data.csv from Cancer Data Kaggle.
+# Output: Pre-trained SVM model
+# This module will be imported by a Web App created by streamlit
+
+
+### Help I got from the internet when creating virtual environment ###
+
+# Gow to create virtual environment on VSCode. 
+# https://www.youtube.com/watch?v=ThU13tikHQw 
+# How to change execution policy if virtual environment cannot be initialised.
+# https://stackoverflow.com/questions/18713086/virtualenv-wont-activate-on-windows
+
+### The Code ###
+
+# Imports
+# Use Command (py -m pip install "package name") to install packages in VScode.
 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -21,13 +40,51 @@ import numpy as np
 import joblib
 from lime import lime_tabular
 
+# Model save name
+
+saveFile = 'Best_SVM_Model.sav'
+
 ## Load the Cancer Dataset
+
 fileName = 'Cancer_Data.csv'
 cancer_dataset = pd.read_csv(fileName)
 raw_dataset = pd.read_csv(fileName)
 
+## Function for the Streamlit Web application
 
-# Classification report and Confusion matrix
+def output(brand_new_dataset):
+    checkForNull, distBM, drop_dataset = EDA(brand_new_dataset)
+    heat_map, distplot, dataset = dataVisualisation(drop_dataset)
+    X, scaled_X_train, scaled_X_test, y_train, y_test, scaler = dataPreperation(dataset)
+    models, fig, message = modelComparison(scaled_X_train, y_train)
+    model_best, tuned_result, report = hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test)
+    modelInterpretation(scaled_X_train, scaled_X_test, model_best, X)
+    joblib.dump(checkForNull, 'checkForNull')
+    joblib.dump(distBM, 'distBM')
+    joblib.dump(heat_map, 'heat_map')
+    joblib.dump(distplot, 'distplot')
+    joblib.dump(X, 'X')
+    joblib.dump(scaler,'scaler')
+    joblib.dump(fig, 'compare_algorithm')
+    joblib.dump(message, 'message')
+    joblib.dump(tuned_result, 'tuned_result')
+    joblib.dump(report, 'report')
+    joblib.dump(dataset, 'cancer_dataset_processed')
+
+## Main function
+
+def main():
+    drop_dataset = EDA(cancer_dataset)[2]
+    dataset = dataVisualisation(drop_dataset)[2]
+    X, scaled_X_train, scaled_X_test, y_train, y_test, scaler = dataPreperation(dataset)
+    models, fig, message = modelComparison(scaled_X_train, y_train)
+    model_best, tuned_result, report = hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test)
+    modelInterpretation(scaled_X_train, scaled_X_test, model_best, X)
+
+
+
+## Function for terminal Classification report and Confusion matrix
+
 def class_report_CM(y_test, y_pred):
 
     accuracy = "Accuracy: "+ str(metrics.accuracy_score(y_test, y_pred))
@@ -43,175 +100,179 @@ def class_report_CM(y_test, y_pred):
     
 
 ## Exploratory data analysis
-# Data explore - First five rows
-first5Row = cancer_dataset.head(5)
-print(first5Row)
 
-# shape
-print(cancer_dataset.shape)
+def EDA(dataset):
+    # Data explore - First five rows
+    dataset.head(5)
 
-# Check data integrity (missing data)
-cancer_dataset.info()
-checkForNull = cancer_dataset.isnull().sum()
-print(checkForNull)
+    # shape
+    print(dataset.shape)
 
-# Distribution of benign and malignant case
-distBM = cancer_dataset.groupby('diagnosis').size()
-print(distBM)
+    # Check data integrity (missing data)
+    dataset.info()
+    checkForNull = dataset.isnull().sum()
+    print(checkForNull)
 
-#List of attributes
-print(cancer_dataset.keys())
+    # Distribution of benign and malignant case
+    distBM = dataset.groupby('diagnosis').size()
+    print(distBM)
+
+    #List of attributes
+    print(dataset.keys())
+
+    # Drop unnecessary data entry for visualisaiton and re-examine the dataset
+    dataset.drop(['id', 'Unnamed: 32'], axis = 1, inplace = True)
+    # Check dataset after modification
+    print(dataset.columns)
+    print(dataset.shape)
+
+    return checkForNull, distBM, dataset
 
 
 ## Data Visualisation
 
+def dataVisualisation(dataset):
 
-# Drop unnecessary data entry for visualisaiton and re-examine the dataset
-cancer_dataset.drop(['id', 'Unnamed: 32'], axis = 1, inplace = True)
-# Check dataset after modification
-print(cancer_dataset.columns)
-print(cancer_dataset.shape)
+    # Replace string B&M to binary 0, 1 so that correlation can be analysed
+    dataset['diagnosis'] = dataset['diagnosis'].replace({'B': 0, 'M':1})
 
-# Replace string B&M to binary 0, 1
-cancer_dataset['diagnosis'] = cancer_dataset['diagnosis'].replace({'B': 0, 'M':1})
+    # Heat map
+    heat_map, ax = plt.subplots(figsize=(20, 20)) #Set size of the heat map
+    sns.heatmap(dataset[dataset.columns].corr(), annot=True, ax = ax)
+    plt.title("Correlation Matrix")
+    plt.show()
 
-# Heat map
-heat_map, ax = plt.subplots(figsize=(20, 20)) #Set size of the heat map
-sns.heatmap(cancer_dataset[cancer_dataset.columns].corr(), annot=True, ax = ax)
-plt.title("Correlation Matrix")
-plt.show()
-
-# Distribution of each attribute
-distplot = plt.figure(figsize = (28,21))
-for feature in range(len(cancer_dataset.columns)):
-    plt.subplot(6, 6, feature + 1)
-    sns.histplot(data = cancer_dataset, x = cancer_dataset.columns[feature], hue = 'diagnosis' )
+    # Distribution of each attribute
+    distplot = plt.figure(figsize = (28,21))
+    for feature in range(len(dataset.columns)):
+        plt.subplot(6, 6, feature + 1)
+        sns.histplot(data = dataset, x = dataset.columns[feature], hue = 'diagnosis' )
+    
+    return heat_map, distplot, dataset
 
 ## Preparing the data for analysis
-# Seperate feature and target set
-X = cancer_dataset.drop('diagnosis', axis = 1) #Everything except the diagnosis column
-y = cancer_dataset['diagnosis'] # The diagnosis column
 
-# Train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+def dataPreperation(dataset):
 
-# Feature scaling
-scaler = StandardScaler()
-scaled_X_train = scaler.fit_transform(X_train)
-scaled_X_test = scaler.transform(X_test)
+    # Seperate feature and target set and Feature scaling
+    scaler = StandardScaler()
+    X = dataset.drop('diagnosis', axis = 1) #Everything except the diagnosis column
+    X_scaled = scaler.fit_transform(X)
+    y = dataset['diagnosis'] # The diagnosis column
 
-model_with_scaled_data = SVC()
-model_with_scaled_data.fit(scaled_X_train, y_train)
-new_y_pred = model_with_scaled_data.predict(scaled_X_test)
-class_report_CM(y_test, new_y_pred)
+    # Train test split
+    scaled_X_train, scaled_X_test, y_train, y_test = train_test_split(
+                                        X_scaled, y, test_size = 0.2, random_state = 42)
+    return X, scaled_X_train, scaled_X_test, y_train, y_test, scaler
 
 
 ## Using k-fold cross validation to test out different model and avoid over-fitting
 
-# Model collection - Spot Check
-models = []
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
+def modelComparison(scaled_X_train, y_train):
+    # Model collection - Spot Check
 
-results = []
-names = []
-message = ""
-for name, model in models:
-    kfold = KFold(n_splits=10, random_state=42, shuffle=True)
-    cv_results = cross_val_score(model, scaled_X_train, y_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    message += msg + '\n'
-    print(msg)
+    models = []
+    models.append(('KNN', KNeighborsClassifier()))
+    models.append(('CART', DecisionTreeClassifier()))
+    models.append(('NB', GaussianNB()))
+    models.append(('SVM', SVC(gamma='auto')))
 
-# Compare Algorithms
-compare_algorithm = plt.figure()
-compare_algorithm.suptitle('Algorithm Comparison')
-ax = compare_algorithm.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
-    # return models
+    results = []
+    names = []
+    message = ""
+    for name, model in models:
+        kfold = KFold(n_splits=10, random_state=42, shuffle=True)
+        cv_results = cross_val_score(model, scaled_X_train, y_train, cv=kfold, scoring='accuracy')
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        message += msg + '\n'
+        print(msg)
+
+    # Compare Algorithms
+
+    compare_algorithm = plt.figure()
+    compare_algorithm.suptitle('Algorithm Comparison')
+    ax = compare_algorithm.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+    plt.show()
+    return models, compare_algorithm, message
 
 ## Hyper parameter tuning for the best result
 
-# def hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test):
-# hyper parameter of current better model
-better_model = models[3][1]
-params = better_model.get_params()
-print(params)
-# Current hyperprameter config
-"""{'C': 1.0, 'break_ties': False, 'cache_size': 200,
-    'class_weight': None, 'coef0': 0.0, 'decision_function_shape': 'ovr',
-    'degree': 3, 'gamma': 'auto', 'kernel': 'rbf', 'max_iter': -1,
-    'probability': False, 'random_state': None, 'shrinking': True, 'tol': 0.001,
-    'verbose': False}"""
+def hypTuning(models, scaled_X_train, scaled_X_test, y_train, y_test):
+    # hyper parameter of current better model
+
+    better_model = models[3][1]
+    params = better_model.get_params()
+    print(params)
+
+    # Current hyperprameter config
+    """{'C': 1.0, 'break_ties': False, 'cache_size': 200,
+        'class_weight': None, 'coef0': 0.0, 'decision_function_shape': 'ovr',
+        'degree': 3, 'gamma': 'auto', 'kernel': 'rbf', 'max_iter': -1,
+        'probability': False, 'random_state': None, 'shrinking': True, 'tol': 0.001,
+        'verbose': False}"""
 
 
-# Since GridSearchCV() is a slow module to run, for the sake of speed,
-# I will put the gridsearch result of the function in params_grid in input so it runs
-# a lot more faster.
-# The intended params_gird looks like this
-# params_grid = {'C':[10, 5, 1, 0.5, 0.1, 0.01],
-#                 'gamma':[0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1],
-#                 'kernel': ['rbf'],
-#                 'probability': [True]}
-# Output = Fitting 20 folds for each of 42 candidates, totalling 840 fits
-# Best Hyper Parameters:  {'C': 10, 'gamma': 0.01, 'kernel': 'rbf', 'probability': True} 
-# Best estimator:  SVC(C=10, gamma=0.01, probability=True)
+    """Since GridSearchCV() is a slow function to run, for the sake of speed,
+    I will put the gridsearch result of the function in params_grid in input so it runs
+    a lot more faster.
+    The intended params_gird looks like this
+    params_grid = {'C':[10, 5, 1, 0.5, 0.1, 0.01],
+                    'gamma':[0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1],
+                    'kernel': ['rbf'],
+                    'probability': [True]}
+    Output = Fitting 20 folds for each of 42 candidates, totalling 840 fits
+    Best Hyper Parameters:  {'C': 10, 'gamma': 0.01, 'kernel': 'rbf', 'probability': True} 
+    Best estimator:  SVC(C=10, gamma=0.01, probability=True)"""
 
+    # Find optimal hyper parameter for this model
+    params_grid = {'C':[10],
+                    'gamma':[0.01],
+                    'kernel': ['rbf'],
+                    'probability': [True]}
+    grs = GridSearchCV(better_model, params_grid, refit = True, verbose = 1, cv = 20)
+    grs.fit(scaled_X_train, y_train)
 
-# Find optimal hyper parameter for this model
-params_grid = {'C':[10],
-                'gamma':[0.01],
-                'kernel': ['rbf'],
-                'probability': [True]}
-grs = GridSearchCV(better_model, params_grid, refit = True, verbose = 1, cv = 20)
-grs.fit(scaled_X_train, y_train)
+    # Display hyper parameter changes
+    tuned_result = grs.best_estimator_
+    tune_result = "\n Best Hyper Parameters: ", grs.best_params_,"\n", "Best estimator: ", grs.best_estimator_,"\n"
+    print (tune_result)
 
-# Display hyper parameter changes
-tuned_result = grs.best_estimator_
-tune_result = "\n Best Hyper Parameters: ", grs.best_params_,"\n", "Best estimator: ", grs.best_estimator_,"\n"
-print (tune_result)
+    # Train model with hyper parameter tuned
+    model_best = grs.best_estimator_
+    y_prediction = model_best.predict(scaled_X_test)
 
-# Train model with hyper parameter tuned
-model_best = grs.best_estimator_
-y_prediction = model_best.predict(scaled_X_test)
-class_report_CM(y_test, y_prediction)
-    # return model_best
+    # Generate report and confusion matrix for web app
+    report = class_report_CM(y_test, y_prediction)
+    cm = confusion_matrix(y_test, y_prediction)
+    disp = ConfusionMatrixDisplay(confusion_matrix = cm)
+    disp.plot()
+    plt.savefig('Confusion_Matrix.png')
+    plt.show()
+    # Save the model for later use in Web App
+    joblib.dump(model_best, saveFile)
 
+    return model_best, tuned_result, report
 
-## Save and load the trained model
-
-saveFile = 'Best_SVM_Model.sav'
-joblib.dump(model_best, saveFile)
-
-# Load the model to make sure it is saved properly
-loaded_model = joblib.load(saveFile)
-final_y_pred = loaded_model.predict(scaled_X_test)
-report = class_report_CM(y_test, final_y_pred)
-cm = confusion_matrix(y_test, final_y_pred)
-disp = ConfusionMatrixDisplay(confusion_matrix = cm)
-disp.plot()
-plt.savefig('Confusion_Matrix.png')
-plt.show()
-
-
-# Performance interpretation using lime,
+## Performance interpretation using lime,
 # with example of the third data in the test set
+def modelInterpretation(scaled_X_train, scaled_X_test, model_best, X):
 
+    exp = lime_tabular.LimeTabularExplainer(scaled_X_train, 
+                                            class_names= ['benign', 'maglinant'], 
+                                            feature_names= X,
+                                            mode = 'classification' )
 
-exp = lime_tabular.LimeTabularExplainer(scaled_X_train, 
-                                        class_names= ['benign', 'maglinant'], 
-                                        feature_names= X,
-                                        mode = 'classification' )
+    explanation = exp.explain_instance(scaled_X_test[2], model_best.predict_proba,
+                                    num_features = len(X),
+                                    top_labels = 2)
 
-explanation = exp.explain_instance(scaled_X_test[2], loaded_model.predict_proba,
-                                num_features = len(X),
-                                top_labels = 2)
+    explanation.as_pyplot_figure()
+    plt.show()
 
-explanation.as_pyplot_figure()
-plt.show()
+## Protect the code from runniing in case of import
+if __name__ == '__main__':
+    main()
--- webAppSVMCancer.py
+++ webAppSVMCancer.py
@@ -1,30 +1,113 @@
-# Use this command in terminal to start streamlit!
+## Author : Zeguo Li
+## Date Created: 26 April 2023
+## Date Last changed 11 May 2023
+
+## This program is a web app that uses cancer.py module to perfrom Machine learning estimation
+#Content: Data showcase, Cell estimator
+# Input: Cancer_data.csv from Cancer Data Kaggle, cancer.py artefacts (model, figures, ndarray etc.)
+# Output: N/A
+# This model utilise Cancer.py
+
+# Use this command in terminal to start streamlit on VSCode!
 # streamlit run webAppSVMCancer.py
 
+### The Code ###
+
+## Imports
+# Use Command (py -m pip install "package name") to install packages in VScode.
 import streamlit as sl
-from cancer import *
+from cancer import raw_dataset, output, saveFile
 from PIL import Image
+import numpy as np
+import pandas as pd
+import joblib
 
+## Load dataset
+dataset = pd.read_csv('Cancer_Data.csv')
+raw_dataset = dataset 
 
+# While loop to load objects/model created by Cancer.py
+# Will not stop until it is sure that everything is loaded
+while True:
+    try:
+        loaded_model = joblib.load(saveFile)
+        checkForNull = joblib.load('checkForNull')
+        distBM = joblib.load('distBM')
+        heat_map = joblib.load('heat_map')
+        distplot = joblib.load('distplot')
+        X = joblib.load('X')
+        scaler = joblib.load('scaler')
+        compare_algorithm = joblib.load('compare_algorithm')
+        message = joblib.load('message')
+        tuned_result = joblib.load('tuned_result')
+        report = joblib.load('report')
+        cancer_dataset = joblib.load('cancer_dataset_processed')
+        break
+    except FileNotFoundError:
+        output(dataset) # First time running this code will be redirected here, cuz objects/model haven't been created yet.
 
 ## streamlit page
 
-# Title and sub title
-sl.title("Cancer Cell Prediction")
-sl.markdown("Predict if cell is cancerous based on its geometric features")
+# Main page
 
-# Two tabs for data visualisation and prediction program
-dsTab, ceTab = sl.tabs(["Data Showcase","Cell estimator"])
+def main():
+    # Title and sub title
+    sl.title("Cancer Cell Prediction")
+    sl.markdown("Predict if cell is cancerous based on its geometric features")
 
-with dsTab:
+    # Two tabs for data visualisation and prediction program
+    dsTab, ceTab = sl.tabs(["Data Showcase","Cell estimator"])
+
+    # Data Showcase Tab
+    with dsTab:
+        dataShowcase()
+
+    # Cell Estimator Tab
+    with ceTab:
+        slider_input = create_sliders()
+        make_prediction(slider_input)
+
+# Prediction function
+
+def make_prediction(sliderList):
+    sliderList = np.array(list(sliderList)).reshape(1, -1) # Convert list to ndarray and lower dimmension
+    sliderList = scaler.transform(sliderList) # Use scaler to scale original input, since the model is trained with scaled data
+    predict = loaded_model.predict(sliderList) # Make prediction based on input
+
+    if predict[0] == 0:
+        sl.markdown("Result: Benign")
+    else:
+        sl.markdown("Result: Malignant")
+        
+    prediction_prob = loaded_model.predict_proba(sliderList) # Show confidence of model prediction
+    proba = prediction_prob[0][predict[0]] # The prediction
+    sl.metric(label = "Confidence", value = "{:.2f}%".format(proba*100),
+                delta = "{:.2f}%".format((proba - 0.5) * 100)) # Show precentage change of confidence compare to indifference
+
+# Create slider and record slider input
+
+def create_sliders():
+    sliderList = []
+    for features in X.columns:  # Generate sliders using loop
+        slider = sl.slider(label = features,
+                            max_value = float(cancer_dataset[features].max()),
+                            min_value = float(cancer_dataset[features].min()),
+                            value = float(cancer_dataset[features].mean()))
+        sliderList.append(slider)
+    return sliderList
+
+# Data visualisation and performance showcase
+
+@sl.cache_data # Streamlit caching function, stores already loaded function into cache so no re-load is needed. Speed up the app.
+def dataShowcase():
     sl.header("Cancer dataset")
     sl.write(raw_dataset)
+
     sl.subheader("Check for missing value")
     sl.write(checkForNull)
 
     sl.subheader("Number of benign and malignant cases in this dataset")
     sl.write(distBM)
-
 
     sl.header("Cancer dataset after cleaning")
     sl.text("Dropped ID and Blank column, convert diagnosis into binary data")
@@ -48,6 +131,10 @@ with dsTab:
     sl.subheader("Confusion Matrix of the model")
     image = Image.open('Confusion_Matrix.png')
     sl.image(image)
+
+if __name__ == '__main__':
+    main()
+
 
 
 
